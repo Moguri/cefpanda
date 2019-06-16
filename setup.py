05fecb4c6a3aca6889ae13cdc46bf9385e1325ba
Mitchell Stokes <mogurijin@gmail.com>
2019-06-15 18:58:47 -0700

    Setup linting with pylint

--- setup.py
+++ setup.py
@@ -11,5 +11,13 @@ setup(
     install_requires=[
         'panda3d',
         'cefpython3',
-    ]
+    ],
+    setup_requires=[
+        'pytest-runner'
+    ],
+    tests_require=[
+        'pytest',
+        'pylint==2.3.*',
+        'pytest-pylint',
+    ],
 )
